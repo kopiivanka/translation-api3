934d00cde4530c2a136119c121f0648c0825c244
Ivanka <ivankakopytsia@gmail.com>
2024-11-11 00:00:28 +0200

    first

---
+++ src/main/java/com/kopytsia/translationapi3/TranslationApi3Application.java
@@ -0,0 +1,13 @@
+package com.kopytsia.translationapi3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslationApi3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranslationApi3Application.class, args);
+    }
+
+}
